3589a6b6e5ea9c4d256fb2ad34e43d0967e152b8
John Kane <john@kanej.me>
2022-05-05 10:21:58 +0100

    add import examples

---
+++ contracts/ArrayCompletion.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract ArrayCompletion {
+    int256[] private arr;
+
+    // function afunction(int256 num) public {
+    //     this.test(123);
+    //     arr.
+    // }
+}
---
+++ contracts/imports/408-outside-project.sol
@@ -0,0 +1,4 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+// import "../../balancer-v2-monorepo/pkg/pool-linear/contracts/LinearMath.sol"; // 408
