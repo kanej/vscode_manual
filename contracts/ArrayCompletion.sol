835f859da627e38a2957e525e841d5462f093ad7
John Kane <john@kanej.me>
2022-05-10 10:58:05 +0100

    update array completion

--- contracts/ArrayCompletion.sol
+++ contracts/ArrayCompletion.sol
@@ -1,11 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract ArrayCompletion {
-    int256[] private arr;
+contract ArrayCompletionBase {
+    int256[] public baseArr;
+}
 
-    // function afunction(int256 num) public {
-    //     this.test(123);
-    //     arr.
-    // }
+contract ArrayCompletion is ArrayCompletionBase {
+    struct Example {
+        int256[] nested;
+    }
+
+    int256[] private stateArr;
+    Example private nestedExample;
+
+    function afunction(int256 num, int256[] memory paramArr)
+        public
+        returns (uint256)
+    {
+        uint256[1] memory local = [uint(1)];
+
+        uint256 l = local.length;
+        stateArr.push(num);
+        nestedExample.nested.pop();
+        baseArr.pop();
+        return paramArr.length + l;
+    }
 }
