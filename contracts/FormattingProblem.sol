8d3c109e9855e4babe546eb1de07d5857ea7af1e
John Kane <john@kanej.me>
2022-08-10 12:26:49 +0100

    Remove warnings

--- contracts/FormattingProblem.sol
+++ contracts/FormattingProblem.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
+// On format the semi-colon after virtual shouldn't be stripped
 abstract contract Foo {
-
+    modifier bar() virtual;
 }
